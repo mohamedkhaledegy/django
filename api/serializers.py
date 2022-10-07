ed7f0c94dfbb31324ef4330782365aa43c5fbd83
mohamedkhaledegy <74407534+mohamedkhaledegy@users.noreply.github.com>
2022-10-07 19:04:19 +0200

    Start App

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers, status
+from .models import *
+
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+
---
+++ api/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework import request, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+
+from rest_framework.authentication import TokenAuthentication
+
+from rest_framework.permissions import  AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly 
+
+from rest_framework.authtoken.models import Token
+#### import serializers
+from .serializers import *
+
+# Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAdminUser,)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        #print(token)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+    #def list(self, request, *args, **kwargs):
+    #    response = {'message': 'You cant create rating like that'}
+    #    return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
